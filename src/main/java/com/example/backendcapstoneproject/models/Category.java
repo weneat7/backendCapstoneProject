a24db706f8dcd0427615e63a0b663c729bc13758
weneat7 <vineetofficial7571@gmail.com>
2023-12-24 21:18:29 +0530

    dasfasdf

---
+++ src/main/java/com/example/backendcapstoneproject/models/Category.java
@@ -0,0 +1,11 @@
+package com.example.backendcapstoneproject.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private Long id;
+    private String name;
+}
